cfdff6b491a81a5b00455487bb19bb3cdaa2d44e
Hannu Kroger <hkroger@gmail.com>
2019-12-17 16:22:48 +0200

    disable user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,11 @@ class RegistrationsController < Devise::RegistrationsController
   ALLOWED_PARAMS = %i(username password password_confirmation email default_client_id pushover problem_report_method reading_error_interval)
   ALLOWED_PARAMS_UPDATE = ALLOWED_PARAMS + %i(current_password) - %i(username)
 
+  # Override to disable temporarily
+  def create
+    raise "Oops, this is disabled!"
+  end
+
   def sign_up_params
     params.require(:user).permit(*ALLOWED_PARAMS)
   end
